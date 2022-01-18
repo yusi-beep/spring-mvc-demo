c8052d02e5820942aa430d9087cdce0bd5aef141
Yusuf Yusufov <yusitoo1234@gmail.com>
2022-01-18 10:30:25 +0200

    Sterting with MVC and adding data to the Spring Model

---
+++ WebContent/view/helloworld-form.jsp
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<title>Hello world -Input Form</title>
+
+</head>
+
+<body>
+
+	<form action="proccessFormVersionTwo" method="GET">
+	
+	<input type="text" name=""studentName 
+		placeholder="What's your name?">
+		
+	</form>
+
+</body>
+
+</html>
---
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,36 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVerisonTwo")
+	public String letsShoutDude(HttpServletRequest request,Model model) {
+		
+		String theName = request.getParameter("studentName");
+		
+		theName = theName.toUpperCase();
+		
+		String result = "Yo! " + theName;
+		
+		//add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+}
